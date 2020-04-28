3bcecb5fc681618fafa7c3c424e21b14b7dc9fcf
nick <nick@ubuntu>
2020-04-28 22:21:42 +0300

    locales

--- app/controllers/acquaint_controller.rb
+++ app/controllers/acquaint_controller.rb
@@ -27,12 +27,12 @@ class AcquaintController < ApplicationController
       acquaint.issue = issue
       if acquaint.valid?
         acquaint.save
-        @journal = Journal.new(journalized: issue, user: User.current)
-        @journal.details << JournalDetail.new(
+        journal = Journal.new(journalized: issue, user: User.current)
+        journal.details << JournalDetail.new(
           property: 'attr',
           prop_key: 'acquaint',
-          value:    "#{acquaint.user.firstname} добавлен в ознакомление #{User.current.name}")
-        @journal.valid? ? @journal.save : nil
+          value:    "#{User.current.name} #{l(:field_add_acquaint_journal)} #{acquaint.user.name}")
+        journal.valid? ? journal.save : nil
       end
     end
 
@@ -47,12 +47,12 @@ class AcquaintController < ApplicationController
     acquaint = Acquaint.find_by_id(params[:id])
     acquaint.status = true;
     acquaint.valid? ? acquaint.save : nil
-    @journal = Journal.new(journalized: Issue.find_by_id(acquaint.issue.id), user: User.current)
-    @journal.details << JournalDetail.new(
+    journal = Journal.new(journalized: Issue.find_by_id(acquaint.issue.id), user: User.current)
+    journal.details << JournalDetail.new(
       property: 'attr',
       prop_key: 'acquaint',
-      value:    "#{acquaint.user.firstname} ознакомлен")
-    @journal.valid? ? @journal.save : nil
+      value:    "#{acquaint.user.name} #{l(:field_acquaint_journal)}")
+    journal.valid? ? journal.save : nil
     respond_to do |format|
       format.html { redirect_to_referer_or {render :html => 'Acquaint create', :status => 200, :layout => true}}
       format.js { render inline: "location.reload();" }
@@ -63,13 +63,13 @@ class AcquaintController < ApplicationController
   def destroy
     acquaint = Acquaint.find_by_id(params[:id])
     acquaint.delete
-    @issue = Issue.find_by_id(acquaint.issue.id)
-    @journal = Journal.new(journalized: @issue, user: User.current)
-    @journal.details << JournalDetail.new(
+    issue = Issue.find_by_id(acquaint.issue.id)
+    journal = Journal.new(journalized: issue, user: User.current)
+    journal.details << JournalDetail.new(
       property: 'attr',
       prop_key: 'acquaint',
-      value:    "#{User.current.name} удалил из ознакомления #{acquaint.user.firstname}")
-    @journal.valid? ? @journal.save : nil
+      value:    "#{User.current.name} #{l(:field_remove_acquaint_journal)} #{acquaint.user.firstname}")
+    journal.valid? ? journal.save : nil
     respond_to do |format|
       format.html { redirect_to_referer_or {render :html => 'Acquaint remove', :status => 200, :layout => true} }
       format.js { render inline: "location.reload();" }
